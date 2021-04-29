5f04675ebf6bf6bb4e70f3da5b29d90f8d59d1ae
NathanRowe32 <Nathandrowe1@gmail.com>
2021-04-29 00:13:40 -0500

    ETHEREUM BLOCKCHAIN SUPPLY CHAIN & EVENT TRIGGERS (TRUFFLE 5)

---
+++ contracts/Item.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.5;
+
+import "./ItemManager.sol";
+
+contract Item {
+    uint public priceInWei;
+    uint public pricePaid;
+    uint public index;
+    
+    ItemManager parentContract;
+    
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
+        parentContract = _parentContract;
+        index = _index;
+        priceInWei = _priceInWei;
+    }
+    
+    receive() external payable {
+        require(pricePaid == 0, "Item is paid already");
+        require(priceInWei == msg.value, "Only full payments allowed");
+        pricePaid += msg.value;
+        (bool success, ) = address(parentContract).call{value: msg.value}(abi.encodeWithSignature("triggerPayment(uint256)", index));
+        require(success, "Transaction voided");
+    }
+}
